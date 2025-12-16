129768238276fddc1163633fb716257626aff251
myj2139 <myj2139@gmail.com>
2025-12-17 04:55:03 +0900

    setting

---
+++ shop-icheon/src/test/java/com/icheon/shop/ShopIcheonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.icheon.shop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopIcheonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
