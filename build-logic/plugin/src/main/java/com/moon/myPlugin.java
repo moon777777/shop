3dda07734ff7331777ab9ab36698ba7a91856075
myj2139 <myj2139@gmail.com>
2025-12-31 14:06:31 +0900

    Convention Plugins setting

---
+++ build-logic/plugin/src/main/java/com/moon/myPlugin.java
@@ -0,0 +1,16 @@
+package com.moon;
+
+import org.gradle.api.Plugin;
+import org.gradle.api.Project;
+
+public class myPlugin implements Plugin<Project>  {
+
+    @Override
+    public void apply(Project project) {
+        project.getPlugins().apply("java");
+        project.getDependencies().add("compileOnly", "org.projectlombok:lombok");
+        project.getDependencies().add("annotationProcessor", "org.projectlombok:lombok");
+        project.getDependencies().add("testImplementation", "org.springframework.boot:spring-boot-starter-test");
+        project.getDependencies().add("testRuntimeOnly", "org.junit.platform:junit-platform-launcher");
+    }
+}
