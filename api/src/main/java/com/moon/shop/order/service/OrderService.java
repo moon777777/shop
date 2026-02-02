f03ebcd6f347a9dc9119c62540dabd6d9db87dc1
myj2139 <myj2139@gmail.com>
2026-02-02 23:51:31 +0900

    OrderService List

--- api/src/main/java/com/moon/shop/order/service/OrderService.java
+++ api/src/main/java/com/moon/shop/order/service/OrderService.java
@@ -3,8 +3,12 @@ package com.moon.shop.order.service;
 import com.moon.shop.order.domain.Order;
 import com.moon.shop.order.dto.request.OrderCreateRequest;
 import com.moon.shop.order.dto.response.OrderCreateResponse;
+import com.moon.shop.order.dto.response.OrderListResponse;
+import com.moon.shop.order.dto.response.OrderSummaryResponse;
 import com.moon.shop.order.repository.OrderRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class OrderService {
@@ -29,6 +33,29 @@ public class OrderService {
                 order.getOrderNumber(),
                 order.getStatus(),
                 "2026-01-01"
+        );
+    }
+
+    public OrderListResponse getMyOrders(int page, int size) {
+
+        List<OrderSummaryResponse> orders = List.of(
+                new OrderSummaryResponse(
+                        1L,
+                        "222-222",
+                        50000,
+                        10000,
+                        40000,
+                        "결제완료",
+                        2,
+                        "2026-01-05"
+                )
+        );
+
+        return new OrderListResponse(
+                page,
+                size,
+                1,
+                orders
         );
     }
 
