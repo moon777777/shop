31a9cf895e32345c0105bc1ad63e20761d7696c3
myj2139 <myj2139@gmail.com>
2026-02-13 17:44:21 +0900

    commit 확인

--- core/src/main/java/com/moon/shop/product/repository/ProductRepository.java
+++ core/src/main/java/com/moon/shop/product/repository/ProductRepository.java
@@ -1,4 +1,9 @@
 package com.moon.shop.product.repository;
 
-public interface ProductRepository {
+import com.moon.shop.order.domain.Order;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Order, Long> {
 }
